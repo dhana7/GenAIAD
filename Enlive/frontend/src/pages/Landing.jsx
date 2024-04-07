d6c9dd7502acfb744a8caaab361e53cb302ac678
SREEVED\sreev <sreevedsup@gmail.com>
2024-04-07 09:36:08 -0500

    commit

--- Enlive/frontend/src/pages/Landing.jsx
+++ Enlive/frontend/src/pages/Landing.jsx
@@ -5,16 +5,13 @@ import {
   MenuUnfoldOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import { Image } from 'antd';
+import { Image } from "antd";
 import { Input } from "antd";
-import {
-  SettingOutlined,
-  SendOutlined,
-} from "@ant-design/icons";
-import { AtomSpinner,BreedingRhombusSpinner } from 'react-epic-spinners'
+import { SettingOutlined, SendOutlined } from "@ant-design/icons";
+import { AtomSpinner, BreedingRhombusSpinner } from "react-epic-spinners";
 import Enlive from "../images/Enlive.png";
 import logo from "../images/logo.png";
-import { Layout, Menu, Button, theme, Avatar, Dropdown, Space } from "antd";
+import { Layout, Menu, Button, theme, Avatar, Dropdown, Space, Card } from "antd";
 import { Col, Row } from "antd";
 import { Brx } from "../services";
 
@@ -25,9 +22,10 @@ const IconFont = createFromIconfontCN({
 const Landing = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [promptState, setPromptState] = useState("");
-  const [displayImage, setDisplayImage] = useState("");
-  const [inputState,setInputState] = useState("")
-  const [loading,setLoading] = useState(false);
+  const [displayImage, setDisplayImage] = useState([]);
+  const [inputState, setInputState] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [getStarted,setGetStarted] = useState(true);
 
   const handleInput = (e) => {
     //   if (e.key === 'Enter') {
@@ -35,10 +33,8 @@ const Landing = () => {
     // };
   };
 
-
   const handlePrompt = (e) => {
     setPromptState(e.target.value);
-    setInputState(e.target.value);
   };
 
   const {
@@ -79,44 +75,6 @@ const Landing = () => {
             }}
             className="text-white"
           />
-<<<<<<< Updated upstream
-          </Row>
-          <Row>
-        <Col span={!collapsed ?8 :24} className='d-flex justify-content-center align-items-center' ><img className='main-logo' src={logo} /></Col>{!collapsed &&<Col className='d-flex justify-content-center align-items-center' span={12} ><img className="demo-logo-vertical" src={Enlive}/></Col> }
-        </Row>
-        <Row className='h-75 d-block'>
-        <Menu
-        theme=''
-          mode="inline"
-          className='bg-primary'
-          defaultSelectedKeys={['1']}
-          items={[
-            getItem('Find templates', 'sub2', <SettingOutlined />, [
-                getItem('Food', '7'),
-                getItem('Education', '8'),
-                getItem('Technology', '9'),
-                getItem('Bussiness', '10'),
-                getItem('Welness', '10'),
-                getItem('Sports', '10'),
-              ]),
-              getItem('Previous creations', 'sub2', <SettingOutlined />, [
-                getItem('Get past 7 days', 'sub2', <SettingOutlined />,[
-                  getItem('Bakery shop', '8'),
-                getItem('Shop improvement', '9'),
-                ]),
-                getItem('Get past 30 days', 'sub2', <SettingOutlined />,[
-                  getItem('Media for bakery', '8'),
-                getItem('My shop', '9'),
-                ]),
-              ]),
-              // getItem('Past 7 days', 'sub2', <SettingOutlined />, [ ]),
-          ]          
-        }
-        
-        />
-          {/* <Row><div className=''>Get past</div></Row> */}
-=======
->>>>>>> Stashed changes
         </Row>
         <Row>
           <Col
@@ -134,42 +92,38 @@ const Landing = () => {
             </Col>
           )}
         </Row>
-        <Row className="h-75 d-block">
+        <Row className="h-75 d-block" style={{
+              overflow: 'auto'
+            }}>
           <Menu
             theme=""
             mode="inline"
             className="bg-primary"
             defaultSelectedKeys={["1"]}
+            
             items={[
-              getItem("Navigation Three", "sub2", <SettingOutlined />, [
-                getItem("Option 7", "7"),
-                getItem("Option 8", "8"),
-                getItem("Option 9", "9"),
-                getItem("Option 10", "10"),
+              getItem("Find templates", "sub1", <SettingOutlined />, [
+                getItem("Food", "7"),
+                getItem("Education", "8"),
+                getItem("Technology", "9"),
+                getItem("Bussiness", "10"),
+                getItem("Welness", "10"),
+                getItem("Sports", "10"),
               ]),
-              getItem("Navigation Three", "sub2", <SettingOutlined />, [
-                getItem("Option 7", "7"),
-                getItem("Option 8", "8"),
-                getItem("Option 9", "9"),
-                getItem("Option 10", "10"),
+              getItem("Previous creations", "sub2", <SettingOutlined />, [
+                getItem("Get past 7 days", "sub3", <SettingOutlined />, [
+                  getItem("Bakery shop", "8"),
+                  getItem("Shop improvement", "9"),
+                ]),
+                getItem("Get past 30 days", "sub4", <SettingOutlined />, [
+                  getItem("Media for bakery", "8"),
+                  getItem("My shop", "9"),
+                ]),
               ]),
+              // getItem('Past 7 days', 'sub2', <SettingOutlined />, [ ]),
             ]}
           />
-        </Row>
-        <Row>
-          <Dropdown overlay={userMenu} trigger={["click"]}>
-            <Button
-              ghost
-              className="bg-transparent border-0 text-white w-auto"
-              onClick={(e) => e.preventDefault()}
-            >
-              <Avatar
-                className="m-2 bg-transparent border-white"
-                icon={<UserOutlined />}
-              />{" "}
-              {!collapsed && "John Doe"}
-            </Button>
-          </Dropdown>
+          {/* <Row><div className=''>Get past</div></Row> */}
         </Row>
       </Sider>
       <Layout className="bg-primary">
@@ -191,52 +145,117 @@ const Landing = () => {
             // minHeight: 280,
             background: colorBgContainer,
             borderRadius: borderRadiusLG,
+            overflowX: "hidden",
+            overflowY: "auto",
           }}
-        
         >
-          <Row className="h-100 w-100 d-flex justify-content-end mb-4" align="bottom">
-          { inputState &&
+          { getStarted &&
           <>
-          <Col className="mb-3">
-          <h4>{inputState} : User <Avatar><UserOutlined /></Avatar></h4>
-          </Col>
+          <div className="z-3 d-flex w-100 h-100 justify-content-center align-items-center">
+            <Card className="shadow-lg tagline">
+            <Row>
+          Boost your ads with NextGenAds: AI-powered precision for unmatched success.
+          </Row>
+          <Row className="d-flex align-items-center justify-content-center mt-3">
+          Click Here to<Button
+                type="primary"
+                className="bg-primary ms-1 d-flex justify-content-center align-items-center "
+                onClick={() => setGetStarted(false)}>
 
-            <Col span={8} className="d-flex w-100 align-content-between me-auto">
-              {loading ?
-              <div className="d-flex justify-content-center align-items-center">
-            <BreedingRhombusSpinner color="#5000A0"/> 
-            </div>
-            :
-            <>
-            <h4 className="w-50 d-flex"><Avatar><UserOutlined /></Avatar>AI :</h4> 
-          <Image
-    width={200}
-    src={displayImage}
-  />   </>}
-  </Col>
-  </>
+                  Get Started
+                </Button>
+          </Row>
+          </Card>
+          </div>
+          </>
 }
-          </Row> 
-          <Row>
-            <Space.Compact className="w-100 d-flex  ">
+          
+            {inputState.length > 0 && (
+              inputState.map((input, index) => (
+              <>
+                <Row
+            className="h-100 w-100 d-flex justify-content-end mb-4"
+            align="bottom"
+          >
+                  <Col className="mb-3">
+                    <div className="message-user tagline">
+                      {input} : User{" "}
+                      <Avatar className="bg-primary border-white">
+                        <UserOutlined />
+                      </Avatar>
+                    </div>
+                  </Col>
+                
+
+                <Col
+                  span={8}
+                  className="d-flex w-100 align-content-between me-auto"
+                >
+                   { displayImage[index] &&
+                      <>
+                        <div className="w-50 d-flex message-ai tagline">
+                          <Avatar>
+                            <UserOutlined />
+                          </Avatar>
+                          AI :
+                        </div>
+                        <Image width={200} src={displayImage[index]} />{" "}
+                      </>
+            }
+                     
+                </Col>
+                </Row>
+              </>
+            ))
+             
+            )}
+          
+          {loading && 
+            <Row>
+            <Col
+            span={24}
+            className="d-flex w-100 align-content-between align-item-center ms-5 mb-5"
+          >
+                      <div className="d-flex h-100 justify-content-center align-items-center">
+                        <BreedingRhombusSpinner size={"50"} color="#5000A0" />
+                      </div> 
+                      </Col>
+                      </Row>
+                    }
+          <Row align={"bottom"} className="position-fixed w-81 d-flex h-88 justify-content-end">
+            <Space.Compact className="w-100 d-flex">
               <Input
                 value={promptState}
                 onChange={handlePrompt}
                 onKeyPress={handleInput}
                 defaultValue="Combine input and button"
+                placeholder="Enter your prompt here (eg: Create a hackathon poster)"
+                disabled = {getStarted}
               />
               <Button
                 type="primary"
+                disabled = {getStarted}
                 className="bg-primary d-flex justify-content-center align-items-center "
                 onClick={() => {
-                  setLoading(true)
-                  setPromptState("")
+                  setLoading(true);
+                  var temTxt = [];
+                  if (inputState.length > 0) {
+                    temTxt = inputState;
+                  }
+                  console.log("prompt", temTxt, promptState);
+                  temTxt.push(promptState);
+                  setInputState(temTxt);
+                  setPromptState("");
                   Brx(promptState).then((data) => {
                     console.log("data", data);
-                    setLoading(false)
-                    setDisplayImage(data)
-                    
-
+                    setLoading(false);
+                    var temImg = [];
+                    if (displayImage.length > 0) {
+                      temImg = displayImage;
+                    }
+                    temImg.push(data);
+                    setDisplayImage(temImg);
+                    console.log(displayImage);
                   });
                 }}
               >
